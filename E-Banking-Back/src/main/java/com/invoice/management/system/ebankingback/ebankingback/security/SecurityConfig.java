b062c4aab6974fceb6fcec5a4e23025c4c2c16a9
Med Elkhalfi <mhammedelkhalfi@gmail.com>
2025-01-01 19:06:41 +0100

    add gaurds and role authorization

--- E-Banking-Back/src/main/java/com/invoice/management/system/ebankingback/ebankingback/security/SecurityConfig.java
+++ E-Banking-Back/src/main/java/com/invoice/management/system/ebankingback/ebankingback/security/SecurityConfig.java
@@ -42,8 +42,8 @@ public class SecurityConfig {
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
         PasswordEncoder passwordEncoder = passwordEncoder();
         return new InMemoryUserDetailsManager(
-                User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
-                User.withUsername("user2").password(passwordEncoder.encode("1234")).roles("USER","ADMIN").build()
+                User.withUsername("user").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("USER","ADMIN").build()
         );
     }
 
